f251586ed639e3ec24abccaa1d97808f52e0fe27
陈风破浪 <754472773@qq.com>
2021-05-20 18:17:58 +0800

    1.首页数据填充 2.分类数据填充

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,59 @@
 // index.js
-// 获取应用实例
-const app = getApp()
+// 引入 用来发送请求的 方法 要补全路径
+import { request } from "../../request/index.js"
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    // 轮播图数组
+    swiperList: [],
+    // 导航数组
+    catesList: [],
+    floorList:[],
   },
   onLoad() {
-    if (wx.getUserProfile) {
+    // 1.发送异步请求获取轮播图数据
+    // wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata', //仅为示例，并非真实接口地址。
+    //   success: (res) => {
+    //     // console.log(res.data);
+    //     this.setData({
+    //       swiperList:res.data.message
+    //     })
+    //   }
+    // });
+
+    // 通过promise 的方式请求数据
+    this.getSwiperList();
+    this.getCateList();
+    this.getFloorList();
+  },
+
+  // 获取轮播图数据
+  getSwiperList(){
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"})
+    .then(result =>{
       this.setData({
-        canIUseGetUserProfile: true
+        swiperList:result.data.message
       })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  // 获取分类导航数据
+  getCateList(){
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"})
+    .then(result =>{
+      this.setData({
+        catesList:result.data.message
+      })
     })
-  }
+  },
+  // 获取楼层数据
+  getFloorList(){
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/floordata"})
+    .then(result =>{
+      this.setData({
+        floorList:result.data.message
+      })
+    })
+  },
+
 })
