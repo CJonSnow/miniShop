1bc559e3e6e3d16d7ccdd5a2dbc5e6ed784a00e9
陈风破浪 <754472773@qq.com>
2021-05-21 17:51:46 +0800

    1.分类详情列表页

--- pages/category/index.js
+++ pages/category/index.js
@@ -14,16 +14,47 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
-    this.getCates();
+    /**
+     * 1. 先判断本地存储中有没有旧的数据
+     *    {time:Date.now(),data:[...]}
+     * 2. 没有旧数据 直接发送新请求
+     * 3. 有旧的数据 且 旧数据也没有过期，直接使用存储中的旧数据即可
+     * 4. web中的本地存储和小程序有什么区别
+     *    web:不管存入的是什么类型的数据，最终都会先调用toString(),把数据变成字符串在存入
+     *    小程序: 没有类型转换这步，存什么类型的数据进去，获取的时候就是什么类型 
+    */
+    // 1. 获取本地存储的数据
+    const Cates = wx.getStorageSync('cates')
+    // 2. 判断
+    if(!Cates){
+      // 不存在 发送请求获取数据
+      this.getCates();
+    }else {
+      // 有旧的数据 定义过期时间
+      if (Date.now()-Cates.time>1000*10) {
+        // 重新发送请求
+        this.getCates();
+      }else {
+        // 可以使用旧的数据
+        this.Cates = Cates.data;
+        let leftMenuList = this.Cates.map(v=>v.cat_name);
+        let rightContent = this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      }
+    }
   },
-
   // 获取分类数据
   getCates(){
     request({
-      url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+      url: "/categories"
     })
     .then(res => {
       this.Cates = res.data.message;
+      //把接口的数据存入到本地存储中
+      wx.setStorageSync('cates', {time:Date.now(),data:this.Cates});
       //左侧菜单数据
       let leftMenuList = this.Cates.map(v=>v.cat_name);
       //右侧内容数据
@@ -45,7 +76,9 @@ Page({
     let rightContent = this.Cates[index].children;
     this.setData({
       currentIndex:index,
-      rightContent
+      rightContent,
+      // 每次点击对右侧scrollTop重新赋值置顶
+      scrollTop:0
     })
   }
 })
